4986def92930e0eeb2179accf6ca899d4b5d3756
vinicvaz <vinicvaz95@gmail.com>
2023-11-27 14:41:07 -0300

    earth image

--- pieces/OpenCVFilterPiece/test_opencv_piece.py
+++ pieces/OpenCVFilterPiece/test_opencv_piece.py
@@ -5,7 +5,7 @@ import pandas as pd
 @skip_envs("github")
 def test_pca_inference_piece():
     input_data = dict(
-        image_path="/home/vinicius/Documents/work/tauffer/ml_domino_pieces/pieces/OpenCVSegmentationPiece/earth.jpg",
-        filter_name="canny",
+        image_path="./pieces/OpenCVFilterPiece/earth.jpg",
+        filter_name="binary",
     )
     output_data = piece_dry_run("OpenCVFilterPiece", input_data)
